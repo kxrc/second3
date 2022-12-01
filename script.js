69c04286f0e432356d6634cf28bbdd7663bff7de
kxrc <79713962+kxrc@users.noreply.github.com>
2022-12-01 02:32:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let btn1 = document.querySelector('#btn1');
+let opros = document.querySelector('.opros');
+
+const random = (min, max) => {
+ const rand = min + Math.random() * (max - min + 1);
+ return Math.floor(rand);
+}
+
+let btn = document.querySelector('#btn');
+btn.addEventListener('mouseenter', () => {
+ btn.style.left = `${random(0, 90)}%`;
+ btn.style.top = `${random(0, 90)}%`;
+})
+
+btn.addEventListener('click', function(){
+ opros.textContent = 'Помилка'
+})
+
+btn1.addEventListener('click', function(){
+ opros.textContent = 'Тіряйся там!'
+})
